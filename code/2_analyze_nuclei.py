f1782e2b60bc5a80b15ce77af774e2182889d4a1
Ekaterina Shitik <shitik.ekaterina@gmail.com>
2024-12-04 23:09:24 +0100

    Adjust code according to PEP8

--- code/2_analyze_nuclei.py
+++ code/2_analyze_nuclei.py
@@ -1,53 +1,18 @@
 #!/usr/bin/env python3
 
 import argparse
-
 import os
-
-from pathlib import Path
-from stardist.models import StarDist2D
-from stardist import random_label_cmap
-from stardist.plot import render_label
-from skimage.io import imread, imsave
-from csbdeep.utils import normalize
-import numpy as np
 from datetime import datetime
+from pathlib import Path
+
+import numpy as np
+from csbdeep.utils import normalize
+from skimage.io import imread, imsave
+from stardist import random_label_cmap
+from stardist.models import StarDist2D
+from stardist.plot import render_label
 
 from validate_folders import validate_path_files
-
-# # Check if the file exists
-# if not os.path.exists(input_json_path):
-#     raise ValueError(f"File '{input_json_path}' does not exist. Please try again.")
-#
-# # Read folder paths from file and convert paths for WSL
-# with open(input_json_path, 'r') as file:
-#     paths_dict = json.load(file)
-#     folder_paths = paths_dict["paths_to_files"]
-#     if folder_paths[0].startswith('C:\\'):
-#         folder_paths = [path.strip().replace('C:\\', '/mnt/c/')
-#                         .replace('\\', '/') for path in folder_paths]
-#
-# # Check existence of folders
-# valid_folders = []
-# for folder_path in folder_paths:
-#     if os.path.exists(folder_path):
-#         valid_folders.append(folder_path)
-#     else:
-#         print(f"Folder '{folder_path}' not found.")
-#
-# print(f"Found {len(valid_folders)} folders for verification.")
-#
-# # Search for Nuclei folder in each folder and determine file types
-# nuclei_folders = []
-# for folder in valid_folders:
-#     nuclei_folder = os.path.join(folder, 'foci_assay', 'Nuclei')
-#     if os.path.exists(nuclei_folder):
-#         files = os.listdir(nuclei_folder)
-#         file_formats = set(os.path.splitext(f)[1] for f in files)
-#         print(f"Nuclei folder found: {nuclei_folder}, File types: {', '.join(file_formats)}")
-#         nuclei_folders.append(nuclei_folder)
-#     else:
-#         print(f"Nuclei folder not found in '{folder}/foci_assay'.")
 
 
 def find_nuclei(nuclei_folders: list) -> None:
@@ -70,16 +35,20 @@ def find_nuclei(nuclei_folders: list) -> None:
     for nuclei_folder in nuclei_folders:
         # Get current date and time in format YYYYMMDD_HHMMSS
         current_datetime = datetime.now().strftime("%Y%m%d_%H%M%S")
-        output_folder_name = f"Nuclei_StarDist_mask_processed_{current_datetime}"
-        output_folder = os.path.join(os.path.dirname(nuclei_folder), output_folder_name)
+        output_folder_name = (f"Nuclei_StarDist_"
+                              f"mask_processed_{current_datetime}")
+        output_folder = os.path.join(os.path.dirname(nuclei_folder),
+                                     output_folder_name)
         Path(output_folder).mkdir(parents=True, exist_ok=True)
 
         # Get list of files with .tif extension
-        image_files = [f for f in os.listdir(nuclei_folder) if f.endswith('.tif')]
+        image_files = [f for f in os.listdir(nuclei_folder)
+                       if f.endswith('.tif')]
 
         # Check if there are any images in the folder
         if not image_files:
-            print(f"No .tif images found in folder '{nuclei_folder}'. Skipping folder.")
+            print(f"No .tif images found in folder "
+                  f"'{nuclei_folder}'. Skipping folder.")
             continue
 
         # Process each image in the folder
@@ -89,7 +58,8 @@ def find_nuclei(nuclei_folders: list) -> None:
 
             # Check if the image is 8-bit grayscale
             if image.dtype != np.uint8:
-                print(f"Image '{image_file}' is not 8-bit grayscale. Skipping file.")
+                print(f"Image '{image_file}' "
+                      f"is not 8-bit grayscale. Skipping file.")
                 continue
 
             # Normalize the image
@@ -123,7 +93,8 @@ def main_analyze_nuclei(input_json_path: str) -> None:
     nuclei_folders = validate_path_files(input_json_path, 2)
 
     # Ask user if analysis should start
-    start_analysis = input("Start analyzing Nuclei folders? (yes/no): ").strip().lower()
+    start_analysis = input("Start analyzing "
+                           "Nuclei folders? (yes/no): ").strip().lower()
     if start_analysis != 'yes':
         raise ValueError("Analysis canceled by user.")
     find_nuclei(nuclei_folders)
