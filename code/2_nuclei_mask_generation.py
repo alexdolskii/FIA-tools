e9320c2cd638b038cc51adc16925c6c7ce1ec4f0
Ekaterina Shitik <shitik.ekaterina@gmail.com>
2025-03-23 13:56:15 +0100

    Correct 2 script and add validation

--- code/2_nuclei_mask_generation.py
+++ code/2_nuclei_mask_generation.py
@@ -6,13 +6,43 @@ import os
 from datetime import datetime
 from pathlib import Path
 
+import imagej
 import numpy as np
 from csbdeep.utils import normalize
+from scyjava import jimport
 from skimage.io import imread, imsave
 from stardist.models import StarDist2D
-import imagej
-from scyjava import jimport
-from validate_folders import validate_path_files
+from validate_folders import validate_input_file
+
+
+def validate_folders(input_json_path: str) -> list:
+    valid_folders = validate_input_file(input_json_path)
+    nuclei_folders = []
+    for folder in valid_folders:
+        # Set up logging
+        file_handler = logging.FileHandler(os.path.join(folder,
+                                                        '2_val_log.txt'),
+                                           mode='w')
+        file_handler.setLevel(logging.WARNING)
+        file_handler.setFormatter(
+            logging.Formatter('%(asctime)s - '
+                              '%(levelname)s - '
+                              '%(message)s'))
+        logging.getLogger('').addHandler(file_handler)
+
+        nuclei_folder = os.path.join(folder,
+                                     'foci_assay',
+                                     'Nuclei')
+        if os.path.exists(nuclei_folder):
+            files = os.listdir(nuclei_folder)
+            file_formats = set(os.path.splitext(f)[1] for f in files)
+            print(f"Nuclei folder found: {nuclei_folder}, "
+                  f"File types: {', '.join(file_formats)}")
+            nuclei_folders.append(nuclei_folder)
+        else:
+            logging.error(f"Nuclei folder not found "
+                          f"in '{folder}/foci_assay'.")
+    return nuclei_folders
 
 
 def find_nuclei(nuclei_folders: list) -> list:
@@ -36,24 +66,33 @@ def find_nuclei(nuclei_folders: list) -> list:
     # Process images in each Nuclei folder
     for nuclei_folder in nuclei_folders:
         # Get current date and time in format YYYYMMDD_HHMMSS
-        current_datetime = datetime.now().strftime("%Y%m%d_%H%M%S")
-        output_folder_name = f"Nuclei_StarDist_mask_processed_{current_datetime}"
-        output_folder = os.path.join(os.path.dirname(nuclei_folder), output_folder_name)
+        current_datetime = (datetime.now()
+                            .strftime("%Y%m%d_%H%M%S"))
+        output_folder_name = (f"Nuclei_StarDist_mask_"
+                              f"processed_{current_datetime}")
+        output_folder = os.path.join(os.path.dirname(nuclei_folder),
+                                     output_folder_name)
         Path(output_folder).mkdir(parents=True, exist_ok=True)
         processed_folders.append(output_folder)
 
         # Setting up logging
-        file_handler = logging.FileHandler(os.path.join(output_folder, '2_log.txt'), mode='w')
+        file_handler = logging.FileHandler(os.path.join(output_folder,
+                                                        '2_log.txt'),
+                                           mode='w')
         file_handler.setLevel(logging.WARNING)
-        file_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+        file_handler.setFormatter(logging.Formatter('%(asctime)s - '
+                                                    '%(levelname)s - '
+                                                    '%(message)s'))
         logging.getLogger('').addHandler(file_handler)
 
         # Get list of files with .tif extension
-        image_files = [f for f in os.listdir(nuclei_folder) if f.endswith('.tif')]
+        image_files = [f for f in os.listdir(nuclei_folder)
+                       if f.endswith('.tif')]
 
         # Check if there are any images in the folder
         if not image_files:
-            logging.error(f"No .tif images found in folder '{nuclei_folder}'. Skipping folder.")
+            logging.error(f"No .tif images found in folder "
+                          f"'{nuclei_folder}'. Skipping folder.")
             continue
 
         # Process each image in the folder
@@ -63,14 +102,18 @@ def find_nuclei(nuclei_folders: list) -> list:
 
             # Check if the image is 8-bit grayscale
             if image.dtype != np.uint8:
-                logging.error(f"Image '{image_file}' is not 8-bit grayscale. Skipping file.")
+                logging.error(f"Image '{image_file}' is "
+                              f"not 8-bit grayscale. "
+                              f"Skipping file.")
                 continue
 
             # Normalize the image
             image = normalize(image)
 
             # Apply model with specified thresholds
-            labels, details = model.predict_instances(image, nms_thresh=0.9, prob_thresh=0.7)
+            labels, details = model.predict_instances(image,
+                                                      nms_thresh=0.9,
+                                                      prob_thresh=0.7)
 
             # Form new file name with _StarDist_processed suffix
             base_name, ext = os.path.splitext(image_file)
@@ -83,7 +126,8 @@ def find_nuclei(nuclei_folders: list) -> list:
     return processed_folders
 
 
-def process_nuclei(valid_folders: list, particle_size: int) -> None:
+def process_nuclei(valid_folders: list,
+                   particle_size: int) -> None:
     """
     Process all files from the provided directories (.tif)
     for the Nuclei channel using ImageJ.
@@ -105,10 +149,13 @@ def process_nuclei(valid_folders: list, particle_size: int) -> None:
     for input_folder in valid_folders:
         # Generate timestamp for the folder name
         current_datetime = datetime.now().strftime("%Y%m%d_%H%M%S")
-        
-        # Create a new folder 'Final_Nuclei_Mask_<timestamp>' in the same directory as input_folder
-        processed_folder = os.path.join(os.path.dirname(input_folder), f'Final_Nuclei_Mask_{current_datetime}')
-        
+
+        # Create a new folder 'Final_Nuclei_Mask_<timestamp>'
+        # in the same directory as input_folder
+        processed_folder = os.path.join(os.path.dirname(input_folder),
+                                        f'Final_Nuclei_Mask_'
+                                        f'{current_datetime}')
+
         if os.path.exists(processed_folder):
             response = input(
                 f"The folder {processed_folder} already exists. "
@@ -123,7 +170,9 @@ def process_nuclei(valid_folders: list, particle_size: int) -> None:
         log_file = os.path.join(processed_folder, 'nuclei_log.txt')
         file_handler = logging.FileHandler(log_file, mode='w')
         file_handler.setLevel(logging.WARNING)
-        file_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+        file_handler.setFormatter(logging.Formatter('%(asctime)s - '
+                                                    '%(levelname)s - '
+                                                    '%(message)s'))
         logging.getLogger('').addHandler(file_handler)
 
         # Valid file extensions
@@ -132,7 +181,9 @@ def process_nuclei(valid_folders: list, particle_size: int) -> None:
         for filename in os.listdir(input_folder):
             # Skip hidden files and files starting with "._"
             if filename.startswith('.') or filename.startswith('._'):
-                logging.warning(f"Skipping hidden or dot-underscore file: {filename}")
+                logging.warning(f"Skipping hidden "
+                                f"or dot-underscore file: "
+                                f"{filename}")
                 continue
 
             # Check file extension
@@ -151,7 +202,9 @@ def process_nuclei(valid_folders: list, particle_size: int) -> None:
             # Open the image
             imp = IJ.openImage(file_path)
             if imp is None:
-                logging.warning(f"Failed to open image: {file_path}. Check Bio-Formats or file integrity.")
+                logging.warning(f"Failed to open image: "
+                                f"{file_path}. "
+                                f"Check Bio-Formats or file integrity.")
                 continue
 
             # Convert image to 8-bit
@@ -178,7 +231,8 @@ def process_nuclei(valid_folders: list, particle_size: int) -> None:
 
             # Save processed image
             base_name = os.path.splitext(filename)[0]
-            output_path = os.path.join(processed_folder, f"{base_name}_processed.tif")
+            output_path = os.path.join(processed_folder,
+                                       f"{base_name}_processed.tif")
             IJ.saveAs(imp_mask, "Tiff", output_path)
             print(f"Processed image saved: {output_path}")
 
@@ -190,13 +244,14 @@ def process_nuclei(valid_folders: list, particle_size: int) -> None:
         IJ.run("Close All")
 
 
-def main(input_json_path: str, particle_size: int) -> None:
+def main(input_json_path: str,
+         particle_size: int) -> None:
     """
     Main function to analyze and process nuclei.
     """
     # Step 1: Analyze nuclei using StarDist
     print("Starting Step 1: Analyzing nuclei with StarDist...")
-    nuclei_folders = validate_path_files(input_json_path, 2)
+    nuclei_folders = validate_folders(input_json_path)
     processed_folders = find_nuclei(nuclei_folders)
     print("Step 1 completed: Nuclei masks created.")
 
@@ -216,7 +271,8 @@ if __name__ == '__main__':
     parser.add_argument('-p',
                         '--particle_size',
                         type=int,
-                        help="Minimum size of nuclei to analyze (in pixels)",
-                        required=True)
+                        help="Minimum size of nuclei to analyze (in pixels). "
+                             "Default is 2500",
+                        default=2500)
     args = parser.parse_args()
     main(args.input, args.particle_size)
